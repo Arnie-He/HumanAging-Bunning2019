d3e1dcedad1ee46b5935dd5382adc7773664b2f5
Arnie-He <arniehe2004@gmail.com>
2023-08-07 15:18:49 -0400

    adjusted for sex and BMI

--- RF Regression/Corrected_dl.py
+++ RF Regression/Corrected_dl.py
@@ -7,3 +7,16 @@ import numpy as np
 import random
 import math
 
+class Model(tf.keras.Model):
+    def __init__(self, *args, **kwargs):
+        super(Model, self).__init__(*args, **kwargs)
+    
+    def call(self, inputs, is_testing = False):
+
+    def loss(self, logits, labels):
+
+    def accuracy(self, logits, labels):
+
+
+def train(model, train_inputs, train_labels):
+    
